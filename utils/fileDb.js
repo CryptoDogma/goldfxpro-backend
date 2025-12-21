fc8c3495e747adb994a1491a4a4709d959804e02
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-21 21:55:29 +0200

    Add ensureFile function to manage file existence

--- utils/fileDb.js
+++ utils/fileDb.js
@@ -1,20 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
-// Render persistent disk
 const BASE_PATH = process.env.RENDER
   ? "/data"
   : path.join(__dirname, "..", "data");
 
-function read(file) {
+function ensureFile(file) {
   const filePath = path.join(BASE_PATH, file);
-  if (!fs.existsSync(filePath)) return [];
-  return JSON.parse(fs.readFileSync(filePath, "utf8"));
+  if (!fs.existsSync(filePath)) {
+    fs.writeFileSync(filePath, "[]");
+  }
+}
+
+function read(file) {
+  ensureFile(file);
+  return JSON.parse(fs.readFileSync(path.join(BASE_PATH, file), "utf8"));
 }
 
 function write(file, data) {
-  const filePath = path.join(BASE_PATH, file);
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+  ensureFile(file);
+  fs.writeFileSync(
+    path.join(BASE_PATH, file),
+    JSON.stringify(data, null, 2)
+  );
 }
 
 module.exports = { read, write };
