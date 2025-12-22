686ce5bc1d0f19d612418d559b4250c2e99b2770
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-22 14:13:55 +0200

    Add emaService for calculating Exponential Moving Average

---
+++ services/emaService.js
@@ -0,0 +1,19 @@
+/**
+ * emaService.js
+ * Calculates Exponential Moving Average
+ */
+
+function calculateEMA(prices, period) {
+  const k = 2 / (period + 1);
+  let ema = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    ema = prices[i] * k + ema * (1 - k);
+  }
+
+  return ema;
+}
+
+module.exports = {
+  calculateEMA
+};
