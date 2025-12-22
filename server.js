2893f6418661edc7967db1af515a696593a40279
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-22 19:44:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use("/api", require("./routes/licenses"));
 app.use("/admin", require("./routes/admin"));
 app.use("/api", require("./routes/signal"));
 app.use("/api", require("./routes/history"));
+app.use("/api", require("./routes/license"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("GOLD FX PRO API running on port", PORT);
 });
+
 
 
 
