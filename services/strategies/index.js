8aa66908ac62452fd74b2417d78c85ee0bd58457
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-23 20:07:32 +0200

    Update index.js

--- services/strategies/index.js
+++ services/strategies/index.js
@@ -1,17 +1,21 @@
-const { runV1 } = require("./v1Strategy");
-const { runV2 } = require("./v2Strategy");
-const { runV3 } = require("./v3Strategy");
+const { runV1 } = require("./v1");
+const { runV2 } = require("./v2");
+const { runV3 } = require("./v3");
 
 async function runStrategy(version, context) {
   switch (version) {
-    case "v3":
-      return runV3(context);
-    case "v2":
-      return runV2(context);
     case "v1":
       return runV1(context);
+    case "v2":
+      return runV2(context);
+    case "v3":
+      return runV3(context);
+    default:
+      return {
+        status: "WAIT",
+        reason: "Unknown strategy version"
+      };
   }
 }
 
 module.exports = { runStrategy };
-
