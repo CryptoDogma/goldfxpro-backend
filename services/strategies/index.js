120bfcff456c1bdb7d4c2cca22f1d568e62c9d89
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-23 17:51:55 +0200

    Update index.js

--- services/strategies/index.js
+++ services/strategies/index.js
@@ -1,14 +1,17 @@
 const { runV1 } = require("./v1Strategy");
 const { runV2 } = require("./v2Strategy");
+const { runV3 } = require("./v3Strategy");
 
 async function runStrategy(version, context) {
   switch (version) {
+    case "v3":
+      return runV3(context);
     case "v2":
       return runV2(context);
     case "v1":
-    default:
       return runV1(context);
   }
 }
 
 module.exports = { runStrategy };
+
