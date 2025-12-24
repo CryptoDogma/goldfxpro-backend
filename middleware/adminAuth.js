85de4aa24268d968f246c24bed631b492f37fe58
CryptoDogma <103742292+CryptoDogma@users.noreply.github.com>
2025-12-24 18:41:55 +0200

    Update adminAuth.js

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,28 +1,14 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config");
+/**
+ * adminAuth.js
+ * Protect admin-only routes using x-admin-secret
+ */
 
 module.exports = (req, res, next) => {
-  const authHeader =
-    req.headers.authorization ||
-    req.headers.Authorization;
+  const secret = req.headers["x-admin-secret"];
 
-  if (!authHeader) {
+  if (!secret || secret !== process.env.ADMIN_SECRET) {
     return res.sendStatus(401);
   }
 
-  // Expect: "Bearer <token>"
-  const parts = authHeader.split(" ");
-
-  if (parts.length !== 2 || parts[0] !== "Bearer") {
-    return res.sendStatus(401);
-  }
-
-  const token = parts[1];
-
-  try {
-    req.user = jwt.verify(token, JWT_SECRET);
-    next();
-  } catch (err) {
-    return res.sendStatus(403);
-  }
+  next();
 };
